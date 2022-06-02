af05e2787b58f2d07a6f64e50824e81dcc7d505e
Seyyu0015 <seyyu0015@163.com>
2022-06-02 11:27:53 +0800

    test:更新测试用例 - 新增中序遍历的测试

--- tree_test.py
+++ tree_test.py
@@ -7,7 +7,23 @@ tree.py
 
 
 def test_tree():
-    tree_node_left = tree.Tree('B')
-    tree_node_right = tree.Tree('C')
+    """pytest 测试"""
+    tree_node_left_left = tree.Tree('D')
+    tree_node_left_right = tree.Tree('E')
+    tree_node_right_left = tree.Tree('F')
+    tree_node_left = tree.Tree('B', tree_node_left_left, tree_node_left_right)
+    tree_node_right = tree.Tree('C', tree_node_right_left)
     tree_test = tree.Tree('A', tree_node_left, tree_node_right)
-    assert tree_test.preorder() == ['A', 'B', 'C']
+    """
+    测试树的结构
+                [A]
+            ↙        ↘
+        [B]             [C]
+      ↙     ↘          ↙
+    [D]     [E]     [F]
+    """
+    # preorder
+    assert tree_test.preorder() == ['A', 'B', 'D', 'E', 'C', 'F']
+
+    # inorder
+    assert tree_test.inorder() == ['D', 'B', 'E', 'A', 'F', 'C']
