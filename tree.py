3dd157c2762db2238a52fba65e1753aac0fbb780
Seyyu0015 <seyyu0015@163.com>
2022-06-06 16:17:33 +0800

    Revert "feat:新增后续遍历postorder()方法"
    This reverts commit 692959d78ac001d451439b3f969c52a284f7d2c4.

--- tree.py
+++ tree.py
@@ -23,8 +23,7 @@ class Tree:
 
     def preorder(self) -> list:
         """
-        前序遍历：
-        首先访问根结点然后遍历左子树，最后遍历右子树。
+        前序遍历首先访问根结点然后遍历左子树，最后遍历右子树。
         在遍历左、右子树时，仍然先访问根结点，然后遍历左子树，最后遍历右子树。
         :return:包含所有节点的item的列表
         """
@@ -48,8 +47,7 @@ class Tree:
 
     def inorder(self) -> list:
         """
-        中序遍历：
-        首先遍历左子树，然后访问根结点，最后遍历右子树。
+        中序遍历首先遍历左子树，然后访问根结点，最后遍历右子树。
         :return:包含所有节点的item的列表
         """
         global NODE_LIST
@@ -69,27 +67,3 @@ class Tree:
             NODE_LIST.append(self.item)
         if self.right is not None:
             self.right.inorder_node()
-
-    def postorder(self) -> list:
-        """
-        后序遍历：
-        首先遍历左子树，然后遍历右子树，最后访问根结点。
-        :return:包含所有节点的item的列表
-        """
-        global NODE_LIST
-        NODE_LIST = []
-        self.postorder_node()
-        return NODE_LIST
-
-    def postorder_node(self):
-        """
-        后序遍历的单个节点,当调用postorder()并创建完成用于记录的NODE_LIST之后
-        此方法会以中序遍历的方式向NODE_LIST末尾添加元素
-        """
-        global NODE_LIST
-        if self.left is not None:
-            self.left.postorder_node()
-        if self.right is not None:
-            self.right.postorder_node()
-        if self.item is not None:
-            NODE_LIST.append(self.item)
